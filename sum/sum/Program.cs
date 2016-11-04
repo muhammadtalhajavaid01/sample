8f96613735abf2611a85155971d29a2294f28dec
M. Talha Javaid <M. Talha Javaid>
2016-11-04 16:39:04 +0500

    sum solution

---
+++ sum/sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    class Program
+    {
+        public double sum (double x,double y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            Program o = new Program();
+            Console.WriteLine("Sum:" + o.sum(2.0, 3.0));
+        }
+    }
+}
